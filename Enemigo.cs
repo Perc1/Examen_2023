195a4a5dbc50fb8a5fd0f5dbeeefce42b50369f7
Perc1 <108903023+Perc1@users.noreply.github.com>
2023-04-18 20:24:48 -0500

    Percy
    Cree clase enemigo, Ishowdata ,items, menu y player

--- Enemigo.cs
+++ Enemigo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Examen
 {
-    class Player : IshowData
+    class Enemigo : IshowData
     {
         string name;
         List<Items> items = new List<Items>();
