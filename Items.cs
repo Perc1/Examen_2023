7fbe3917f820cf952c85710fe1f124b976689c40
Catipo90 <sthefano90_@hotmail.com>
2023-04-18 19:57:06 -0500

    Sthefano Catpo -- Clase de Jugador con datos / Clase de items y NPC

---
+++ Items.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    class Items
+    {
+        public string name;
+        float price;
+        public string itemtype;
+        
+
+        public Items(string name, string itemtype, float price)
+        {
+            this.name = name;
+            this.itemtype = itemtype;
+            this.price = price;
+        }
+
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    class Player : IntShowData
+    {
+        string name;
+        List<Items> items = new List<Items>();
+        float experience;
+        int level;
+        float money;
+
+        public void GettingItem(Items item)
+        {
+            items.Add(item);
+        }
+
+        public virtual string GettingName(string name)
+        {
+            this.name = name;
+            return name;
+        }
+
+        public virtual float GettingExperience(float experience)
+        {
+            this.experience = experience;
+            return experience;
+        }
+
+        public virtual float GettingLevel(int level)
+        {
+            this.level = level;
+            return level;
+        }
+
+        public string ShowData()
+        {
+            return "Nombre: " + name + " " + "Experiencia: " + experience + " " + "Nivel: " + level;
+        }
+
+    }
+
+}
+
